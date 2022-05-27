608ab67baa3571971d95592a052f1cfae471ff07
Emre Dağ <ewredag@gmail.com>
2022-05-28 00:50:33 +0300

    Header created.

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "../constants/icons/logo.png";
+
+function Header() {
+	return (
+		<div className='header'>
+
+			<img src={logo} alt="Rise Tech" />
+        
+		</div>
+	);
+}
+
+export default Header;
--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Footer from "../components/Footer";
+import Header from "../components/Header";
 
 
 function IndexPage() {
 	return (
 		<div className="index-page">
+			<Header/>
 			<Footer/>
 		</div>
 	);
