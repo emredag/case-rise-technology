2fd13ae26cc4afcc0c155dfb179bed0c00a37fce
Emre Dağ <ewredag@gmail.com>
2022-05-28 00:37:50 +0300

    Footer finished.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,16 @@ import React from "react";
 function Footer() {
 	return (
 		<div className='footer'>
-			<div className="git">asdasd</div>
-			<div className='copyright'>@2022 Emre Dağ</div>
+
+			<div className="git">
+				<div className="box">git</div>
+				<div className="repo">
+					<a href="https://github.com/emredag/case-rise-technology" target="_blank" rel="noreferrer">repository</a>
+				</div>
+			</div>
+
+			<div className='copyright'>Ⓒ2022 Emre Dağ</div>
+
 		</div>
 	);
 }
